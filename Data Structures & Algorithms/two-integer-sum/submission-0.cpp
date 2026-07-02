289b7d522b721ed00a693cc16104829deee52407
Srimadhavan G <sg8081@srmist.edu.in>
2026-07-03 01:47:55 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> freq;
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]] = i;
+        }
+
+        for(int i=0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if (freq.count(complement) && freq[complement] != i){
+                return {i, freq[complement]};
+            }
+        }
+        return {};
+
+    }
+};
